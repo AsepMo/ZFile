726eaf2549230cce1367fb9e3f5075891282dfe2
AsepMo <asepmo.story@gmail.com>
2020-07-04 00:09:33 +0700

    Update Data ZFile

---
+++ DataApplication/app/src/main/java/com/store/data/MainActivity.java
@@ -0,0 +1,14 @@
+package com.store.data;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
